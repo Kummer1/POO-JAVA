a1e204b7ac532147e1e2c2eb2124ec206da8ee98
Kummer1 <gustavo.kummer1@gmail.com>
2025-05-17 11:45:27 -0300

    Adição calculadora de tempo e serie

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,7 @@
+import calculos.CalculadoraDeTempo;
 import modelo.Filme;
+import modelo.Serie;
+import calculos.CalculadoraDeTempo;
 
 public class Principal {
     public static void main(String[] args) {
@@ -20,5 +23,28 @@ public class Principal {
         //System.out.println("Soma das avaliações: "+ meuFilme.somaDasAvaliacoes);
         //System.out.println("Total de avaliações "+ meuFilme.totalAvaliacoes);
         //System.out.println(String.format("Media das avaliações: %.2f ", meuFilme.pegaMedia()));
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.setTemporadas(15);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Minutos para terminar lost: " + lost.getDuracaoEmMinutos());
+
+        Serie blackMirror = new Serie();
+        blackMirror.setNome("BlackMirror");
+        blackMirror.setTemporadas(7);
+        blackMirror.setEpisodiosPorTemporada(12);
+        blackMirror.setMinutosPorEpisodio(55);
+        blackMirror.setAnoDeLancamento(2021);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(blackMirror);
+        calculadora.inclui(lost);
+
+        System.out.println("O tempo total para terminar as series e filmes é: " + calculadora.getTempoTotal());
     }
+
 }
---
+++ src/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,19 @@
+package calculos;
+import modelo.Filme;
+import modelo.Serie;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+    public void inclui(Filme f){
+        tempoTotal += f.getDuracaoEmMinutos();
+    }
+
+    public void inclui(Serie s){
+        tempoTotal += s.getDuracaoEmMinutos();
+    }
+}
